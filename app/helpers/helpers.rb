1e6d9d60c8ceb24447a835c98c30e2c690dafc7a
Fiona Lapham <fionadlapham@gmail.com>
2018-07-11 11:31:30 +0000

    automatically backed up by learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -3,8 +3,7 @@
 class Helpers
   
   def current_user(session)
-    current_user =User.find(session[:user_id])
-    current_user
+    current_user = User.find(session[:user_id])
   end
 
   def is_logged_in?
